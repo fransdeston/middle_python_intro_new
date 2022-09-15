5d68dfa1309df2ac8725594e6877aadad951566c
Getta Alex <fransdestonbl4@gmail.com>
2022-09-15 17:58:51 +0300

    Работа с Flake8 жесткая!)

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 """Генератор приветствий."""
-import pprint
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
+
+def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
 
       Args:
           name: Имя пользователя
 
       Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+          str: Текст приветствия
+    """
+    return f'Привет, {name.title()}'
